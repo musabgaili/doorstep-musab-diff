1fc9cf3af717d1434db9499b6ab56023f858f448
elmu35b <musabgaili@gmail.com>
2025-01-11 15:00:39 +0300

    redirect

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    $user = User::first();
-    Mail::raw('This is a test email', function ($message) use ($user) {
-        $message->to('musabgaili@gmail.com')
-                ->subject('Test Email')
-                ->from('hello@musab.link');
-    });
-    return 'done';
+    // $user = User::first();
+    // Mail::raw('This is a test email', function ($message) use ($user) {
+    //     $message->to('musabgaili@gmail.com')
+    //             ->subject('Test Email')
+    //             ->from('hello@musab.link');
+    // });
+    // return 'done';
+    return redirect()->away('https://doorstepview.com/');
+
 
 });
